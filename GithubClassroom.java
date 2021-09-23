50504ae6ade3f3f6436eeea1e8b752b8b29c31f9
Mr. Mueller <bmueller@hstat.org>
2021-09-23 12:53:14 +0000

    starter code/instructions

---
+++ GithubClassroom.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+class GithubClassroom {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        // make this print "Hello Github Classroom"
+        // make sure you can run it in your terminal!
+        System.out.println("Hello"); 
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # github-classroom
+
+Clone this into your `u00-other` folder. Follow the directions on `GithubClassroom.java` then push it back to Github.
